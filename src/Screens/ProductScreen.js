46368125f2fc6595659b84793b282cb29724cac6
Hemal <h.githmin@gmail.com>
2021-01-10 12:59:58 +0530

    add redux to produx screen

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
-import Product from '../components/Product'
+import { detailsProduct } from '../actions/productAction';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data'
 
 export default function ProductScreen(props) {
-    const product = data.products.find((x)=>x._id === props.match.params.id);//array.find(function(currentValue, index, arr),thisValue) ->The find() method returns the value of the first element in an array that pass a test (provided as a function)
-    if(!product){
-        return <div>Product Not Found</div>
-    }
+   const dispatch = useDispatch(); 
+   const productId =props.match.params.id;
+   // const product = data.products.find((x)=>x._id === props.match.params.id);//array.find(function(currentValue, index, arr),thisValue) ->The find() method returns the value of the first element in an array that pass a test (provided as a function)
+   const productDetails=useSelector(state => state.productDetails);
+   const {loading,error,product} =productDetails;
+
+   useEffect(() => {
+        dispatch(detailsProduct(productId));
+   },[dispatch,productId])
+ 
     return (
         <div>
+        { loading?(<LoadingBox></LoadingBox>)
+        :error?(<MessageBox variant="danger">{error}</MessageBox>)
+        :(
+            <div>
             <Link to="/">Back to result</Link>
             <div className="row top">
                 <div className='col-2'>
@@ -64,5 +76,11 @@ export default function ProductScreen(props) {
 
             </div>
         </div>
+        )
+        }
+    
+      </div> 
+
+        
     )
 }
--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,15 +1,30 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS,PRODUCT_DETAILS_FAIL,PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_REQUEST } from "../constants/productConstants";
 
-const productListReducer =(state ={loading:true,products:[]}/*defualt state -meka nattan error ekak enwa*/,action)=>{
+export const productListReducer =(state ={loading:true,products:[]}/*defualt state -meka nattan error ekak enwa*/,action)=>{
     switch(action.type){
         case PRODUCT_LIST_REQUEST:
             return {loading:true}//here we send ajax to back end and waiting for response
         case PRODUCT_LIST_SUCCESS:
-            return {loading:false,products:action.payload}//products kiyanne redux store eke tiyana variable ekak methandi karann ebackend eken ganna data redux store ekata dana eka
+            return {loading:false,products:action.payload}//state eke tiyana products kiyana array ekata danne//products kiyanne redux store eke tiyana variable ekak methandi karann ebackend eken ganna data redux store ekata dana eka
         case PRODUCT_LIST_FAIL:
             return {loading:false,error:action.payload}
         default:
             return state;  //dont change state ,return the previouse state
     }
 }
-export default productListReducer;
+
+
+
+export const productDetailsReducer=(state={product:{},loading:true},action)=>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading:true}
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading:false,product:action.payload}//state eke tiyan product kiyana object ekata danne
+        case PRODUCT_DETAILS_FAIL:
+            return {loading:false,error:action.payload}
+        default:
+            return state;
+
+    }
+}
